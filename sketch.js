3c46a7bff51855380ddbd17256a407e96f60c287
Roast Gameez <70948157+OM007VK@users.noreply.github.com>
2021-05-05 15:29:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function setup() {
    wall = createSprite(1200, 200, thickness, 100);
 
    speed=random(223,321);
-   weight=random(30,52);
+   //weight=random(30,52);
 
    bullet.velocityX = speed;
 
@@ -23,38 +23,27 @@ function draw() {
  
    background("black");
 
-   if (wall.x-bullet.x < (bullet.width+wall.width)/2){
-       bullet.velocityX=0;
-   var deformation=0.5 * weight * speed/22509;
-   if (deformation>180) {
-       bullet.shapeColor = "white";
-   }
-   if (deformation>180 && deformation>100) {
-    bullet.shapeColor = "white";
-   }
-   if (deformation>100) {
-    bullet.shapeColor = "white";
-  }
-}
-
+    var damage=0.5 * speed* speed/(thickness *thickness *thickness);
+    textSize(20)
+    fill("white")
+    text('damage:' +damage ,100,300);
+    
 if(hasCollided(bullet, wall))
 {
   bullet.velocityX=0;
-  var damage=0.5 * speed* speed/(thickness *thickness *thickness);
+
 
   if (damage>1.5)
   {
    wall.shapeColor="red";
   }
-   if (damage<1.5)
+   if (damage<=1.5)
   {
   wall.shapeColor="green";
   }
 }
-
-
- drawSprites();
-
+    bullet.x = wall.x-(bullet.width+wall.width)/2
+  drawSprites(); 
 }
 
 
